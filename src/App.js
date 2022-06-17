8e7305ff30fd13bb40944f40408de7cb7c036e22
antmensch <105740241+antmensch@users.noreply.github.com>
2022-06-17 17:33:55 +0300

    task submission

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
-import logo from './logo.svg';
+import Header from "./components/Header"
 import './App.css';
+import Card from "./components/Card";
+import data from "./data/data"
+
 
 function App() {
   return (
     <div className="App">
-      APp component
+      <Header />
+      <div className="cardContainer">
+        {data.map(function(item, index) {
+          return (
+            <Card item={item} key={`card_${index}`} index={index} />
+          )
+        })}
+      </div>
     </div>
   );
 }
---
+++ src/data/data.js
@@ -0,0 +1,32 @@
+export default [
+    {
+        title: "Lviv City",
+        location: "Ukraine",
+        gMapsLink: "https://goo.gl/maps/PtTciNoRdzfLt7CCA",
+        startDate: "12 Jan, 2021",
+        endDate: "24 Jan, 2021",
+        description: "Lviv is the largest city in Western Ukraine, and the sixth-largest in Ukraine, with a population of 717,510 Lviv is one of the main cultural centres of Ukraine. It was named in honour of Leo, the eldest son of Daniel, King of Ruthenia. ",
+        imageURL: "lviv.jpg",
+    },
+
+    {
+        title: "Machu Picchu",
+        location: "Peru",
+        gMapsLink: "https://goo.gl/maps/hbWGPkMijwymEF648",
+        startDate: "12 May, 2021",
+        endDate: "24 May, 2021",
+        description: "Machu Picchu is a 15th-century Inca citadel located in the Eastern Cordillera of southern Peru on a 2,430-meter (7,970 ft) mountain ridge. It is located in the Machupicchu District within Urubamba Province above the Sacred Valley",
+        imageURL: "machu.jpg",
+    },
+
+    {
+        title: "Lake Nakuru",
+        location: "Kenya",
+        gMapsLink: "https://goo.gl/maps/U7u9K3WTMGHSoaCn7",
+        startDate: "12 Oct, 2021",
+        endDate: "24 Oct, 2021",
+        description: "Lake Nakuru is one of the Rift Valley lakes at an elevation of 1,754 m (5,755 ft) above sea level. It lies to the south of Nakuru, in the rift valley of Kenya and is protected by Lake Nakuru National Park.",
+        imageURL: "nakuru.jpg",
+    }
+]
+    
